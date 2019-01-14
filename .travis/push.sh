ffc469be310f84c30370392da25da5ec927190ef
Kyrre Havik Eriksen <kyrremann@gmail.com>
2019-01-14 23:48:12 +0100

    Tror jeg har riktig git-kommando

--- .travis/push.sh
+++ .travis/push.sh
@@ -5,17 +5,17 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-commit_website_files() {
-  git checkout -b master
+commit_files() {
+  git checkout master
   git add *.html _monthly/ _data/
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GITHUB_TOKEN}@github.com/Kyrremann/allmy.beer.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin-pages master
+  git remote set-url origin https://${GITHUB_TOKEN}@github.com/Kyrremann/allmy.beer.git
+  git push
 }
 
 setup_git
-commit_website_files
+commit_files
 upload_files
